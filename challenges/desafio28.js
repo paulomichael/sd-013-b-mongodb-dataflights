a025279c3a4e8a25f3bec6a5518484ca6f6773ae
Felipe Ventorim <felipeventorim11@gmail.com>
2021-11-17 14:18:11 -0300

    Desafio28

---
+++ challenges/desafio28.js
@@ -0,0 +1,22 @@
+const LATAM = "LATAM AIRLINES BRASIL";
+
+db.resumoVoos.insertOne({
+  empresa: LATAM,
+  totalVoosDomesticos: db.voos
+    .find({
+      "empresa.nome": LATAM,
+      natureza: "Doméstica",
+    })
+    .count(),
+});
+
+db.resumoVoos.findOne(
+  {
+    empresa: LATAM,
+  },
+  {
+    empresa: 1,
+    totalVoosDomesticos: 1,
+    _id: 0,
+  },
+);
